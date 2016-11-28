e978804003928fbd450b3cb40bff5f8780de71f6
CEBEP86 <blackmesa944@gmail.com>
2016-11-28 16:24:54 +0200

    Пробуем Hibvernate

--- src/main/java/ApplicationTask/TaskRepository.java
+++ src/main/java/ApplicationTask/TaskRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
+    @Autowired
+    TaskRepository taskRepository;
+
 
 }
