2f0e326e27baec79f44b97223c490ab25222d835
CEBEP86 <blackmesa944@gmail.com>
2016-11-29 11:16:54 +0200

    подключил Hibernate - меньше ошибок

--- src/main/java/io/sever86/tasks/Config.java
+++ src/main/java/io/sever86/tasks/Config.java
@@ -11,6 +11,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by Администратор on 24.11.2016.
@@ -18,15 +21,29 @@ import javax.sql.DataSource;
 @Configuration
 @EnableJpaRepositories(basePackages = "io.sever86.tasks")
 @EnableTransactionManagement
-public class Config {
+public class Config  {
 
     @Bean
     public DataSource dataSource() {
         final HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5433/TaskApplication");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setUsername("ii");
-        config.setPassword("ii");
+        Properties property = new Properties();
+        try {
+            FileInputStream fis;
+            fis = new FileInputStream("src/main/resources/application.properties");
+            property.load(fis);
+
+            String host = property.getProperty("spring.datasource.url");
+            String login = property.getProperty("spring.datasource.username");
+            String password = property.getProperty("spring.datasource.password");
+            String driver = property.getProperty("spring.datasource.driver-class-name");
+
+            config.setJdbcUrl(host);
+            config.setDriverClassName(driver);
+            config.setUsername(login);
+            config.setPassword(password);
+        }catch (IOException e) {
+            System.err.println("ОШИБКА: Файл свойств отсуствует!");
+        }
 
         return new HikariDataSource(config);
     }
