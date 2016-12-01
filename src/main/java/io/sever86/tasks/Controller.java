4260701c26f63feced5355fb8cc50b49edfc0e7f
CEBEP86 <blackmesa944@gmail.com>
2016-12-01 13:31:13 +0500

    очистил pom

--- src/main/java/io/sever86/tasks/Controller.java
+++ src/main/java/io/sever86/tasks/Controller.java
@@ -19,30 +19,21 @@ public class Controller {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    /*
-      public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) { ///обработка сигнала in
-          System.out.println(name);
-          return new Greeting(counter.incrementAndGet(),
-                  String.format(template, name));
-      }  */
-////////////////////////////////
+
     @RequestMapping(method = RequestMethod.POST, path = "/save") ///адрес который принимает запросы
     public String save(@RequestBody List<Task> text) {
-        for(Task a:text)
-        TaskDao.save(a);
+        for (Task a : text)
+            TaskDao.save(a);
 
-         return "Ok";
+        return "Ok";
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/load") ///адрес который принимает запросы
     public List<Task> load(@RequestBody String text) {
 
-       System.out.println( TaskDao.load());
+        System.out.println(TaskDao.load());
         return TaskDao.load();
-        }
-
-
-
+    }
 
 
 }
