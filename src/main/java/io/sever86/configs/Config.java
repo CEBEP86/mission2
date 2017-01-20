98acbc759b242b3081cd437a9c539a9df5c5c225
CEBEP86 <blackmesa944@gmail.com>
2017-01-20 10:32:07 +0500

    SpringSecurityBegin

--- src/main/java/io/sever86/configs/Config.java
+++ src/main/java/io/sever86/configs/Config.java
@@ -2,12 +2,7 @@ package io.sever86.configs;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import io.sever86.dao.ExecutorDao;
-import io.sever86.dao.PersonalDao;
-import io.sever86.dao.TaskDao;
-import io.sever86.dao.TaskDaoJdbcTemplate;
-import io.sever86.dao.ExecutorDaoJdbcTemplate;
-import io.sever86.dao.PersonalDaoJdbcTemplate;
+import io.sever86.dao.*;
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -53,12 +48,16 @@ public class Config {
     TaskDao taskDao() {
         return new TaskDaoJdbcTemplate();
     }
+
     @Bean
     ExecutorDao executorDao() {
         return new ExecutorDaoJdbcTemplate();
     }
+
     @Bean
-    PersonalDao personalDao() {  return new PersonalDaoJdbcTemplate();    }
+    PersonalDao personalDao() {
+        return new PersonalDaoJdbcTemplate();
+    }
 }
 
 
